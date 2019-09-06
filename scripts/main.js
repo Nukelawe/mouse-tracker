af501e15c57c68e9c249a0fdda77eb25ec66f39b
Nukelawe <nukelawe@gmail.com>
2019-09-06 18:02:06 +0300

    added mouse event data colleciton

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const min_width = 15;
 const max_width = 100;
-const game_length = 43;
+const game_length = 3;
 
 var canvas = document.getElementById('canvas');
 var results = document.getElementById('results');
@@ -13,8 +13,9 @@ var missClick = false;
 var clickCount = 0;
 var missClickCount = 0;
 var startTime = null;
-
-var target = { x: 0, y: 0, width: 0 };
+var target = null;
+var data = null;
+var path = null;
 
 function startGame() {
 	canvas.style.display = "block";
@@ -22,6 +23,7 @@ function startGame() {
 	timeScore.style.display = "none";
 	missClickScore.style.display = "none";
 
+	data = [];
 	clickCount = 0;
 	missClickCount = 0;
 	startTime = Date.now().valueOf();
@@ -32,26 +34,26 @@ function endGame() {
 	results.style.display = "block";
 	timeScore.style.display = "block";
 	missClickScore.style.display = "block";
-	setResults();
-	target.width = 0;
+	let time = Date.now().valueOf() - startTime;
+	document.getElementById("time").innerHTML = "Time: " + (time / 1000 + missClickCount);
+	document.getElementById("missclicks").innerHTML = "Miss clicks: " + missClickCount;
+	console.log(data);
+	target = null;
+	data = null;
+	path = null;
 }
 
 function newTarget() {
-	target.width = min_width + Math.floor(Math.random() * (max_width - min_width));
-	target.x = Math.floor(Math.random() * (canvas.clientWidth - target.width));
-	target.y = Math.floor(Math.random() * (canvas.clientHeight - target.width));
-}
-
-function setResults() {
-	let timeResult = document.getElementById("time");
-	let missClickResult = document.getElementById("missclicks");
-	let time = Date.now().valueOf() - startTime;
-	timeResult.innerHTML = "Time: " + (time / 1000 + missClickCount);
-	missClickResult.innerHTML = "Miss clicks: " + missClickCount;
+	width = min_width + Math.floor(Math.random() * (max_width - min_width));
+	target = {
+		width: width,
+		x: Math.floor(Math.random() * (canvas.clientWidth - width)),
+		y: Math.floor(Math.random() * (canvas.clientHeight - width))
+	};
 }
 
 function render() {
-	if (clickCount >= game_length) endGame();
+	if (target == null) return;
 	context.fillStyle = missClick ? '#5a1e1e' : '#000000';
 	context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 	context.fillStyle = '#ffffff';
@@ -60,28 +62,50 @@ function render() {
 	context.fillRect(target.x+1, target.y+1, target.width-2, target.width-2);
 }
 
+function onMouseEvent(event) {
+	if (path == null) return;
+	let canvasRect = canvas.getBoundingClientRect();
+	let x = event.clientX - canvasRect.x;
+	let y = event.clientY - canvasRect.y;
+	path.push({
+		type: event.type,
+		timestamp: event.timeStamp,
+		x: x - (target.x + target.width/2),
+		y: y - (target.y + target.width/2)
+	});
+}
+
 document.onmousedown = function(event) {
-	let rect = canvas.getBoundingClientRect();
-	let x = event.clientX - rect.x;
-	let y = event.clientY - rect.y;
-	if (target.width === 0) {
+	onMouseEvent(event);
+	let canvasRect = canvas.getBoundingClientRect();
+	let x = event.clientX - canvasRect.x;
+	let y = event.clientY - canvasRect.y;
+
+	if (path == null) {
 		startGame();
+		path = [];
 		newTarget();
 	} else if (x >= target.x && x <= target.x + target.width && y >= target.y && y <= target.y + target.width) {
+		data.push({
+			path: path,
+			target: target
+		});
+		path = [];
 		++clickCount;
 		newTarget();
 	} else {
 		++missClickCount;
 		missClick = true;
 	}
+
+	if (clickCount >= game_length) endGame();
 	render();
 };
-
 document.onmouseup = function(event) {
+	onMouseEvent(event);
 	missClick = false;
 	render();
 }
-
 document.onmousemove = function(event) {
-	console.log(event);
+	onMouseEvent(event);
 }
